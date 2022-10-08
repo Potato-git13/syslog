70dfb4778a2b5a38cbeb82d058ed7d28693b364c
potato <lanveble08@gmail.com>
2022-10-08 19:18:14 +0200

    Added a function for finding variables in a file and refactored the cpu_usage function

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 #define SIZE 64
 
@@ -11,11 +12,31 @@ int asprintf(char **strp, const char *fmt, ...);
 typedef struct _log_entry {
     char *date;
     char *time;
-    int cpuuse;
-    int ramuse;
-    int bat;
+    unsigned int cpuuse;
+    unsigned int ramuse;
+    unsigned int bat;
     char *uptime;
 } log_entry;
+
+int pscanf(const char *path, const char *fmt, ...){
+	FILE *fp;
+	va_list ap;
+	int n;
+
+    // Open file and check pointer
+    fp = fopen(path, "r");
+	if (!fp) {
+		return -1;
+	}
+
+    // Scan the file
+	va_start(ap, fmt);
+	n = vfscanf(fp, fmt, ap);
+	va_end(ap);
+	fclose(fp);
+
+	return (n == EOF) ? -1 : n;
+}
 
 void get_time(log_entry *entry){
     time_t t = time(NULL);
@@ -35,46 +56,24 @@ void get_time(log_entry *entry){
 void cpu_usage(log_entry *entry){
     char *token, *str, *tofree;
     int int_token;
-    int count = 0;
+    unsigned int count = 0;
 
-    int all_sums[2] = {0x0};
-    int work_sums[2] = {0x0};
+    unsigned int all_sums[2], work_sums[2] = {0x0};
+    long double vals[14] = {0x0};
     float all_over_time, work_over_time;
-    int cpuperc;
+    unsigned int cpuperc;
 
-    // Open the file, read the first line, sum all and work valuesž
-    // Repeat 2x 1 sec apart to get different values
+    // Get two sets of values 1 sec apart
     for(int i = 0; i <= 1; i++){
-        FILE *fp;
-        fp = fopen("/proc/stat", "r");
-        char *line;
-        size_t len = 0;
-
-        // Check file pointer
-        if (!fp){
+        if (pscanf("/proc/stat", "%*s %Lf %Lf %Lf %Lf %Lf %Lf %Lf",
+		           &vals[0+i*7], &vals[1+i*7], &vals[2+i*7], &vals[3+i*7], &vals[4+i*7], &vals[5+i*7], &vals[6+i*7])
+                   != 7){
             return;
-        }
-        // Get the first line
-        getline(&line, &len, fp);
+        };
 
-        tofree = str = strdup(line);
-        while ((token = strsep(&str, " "))){
-            if (!strcmp(token, "cpu") || *token == '\0' || *token == '\n'){
-                continue;
-            }
-            // Convert the token to int
-            int_token = strtol(token, NULL, 10);
+        all_sums[i] = vals[0+i*7] + vals[1+i*7] + vals[2+i*7] + vals[3+i*7] + vals[4+i*7] + vals[5+i*7] + vals[6+i*7];
+        work_sums[i] = vals[0+i*7] + vals[1+i*7] + vals[2+i*7];
 
-            all_sums[i] += int_token;
-            // Add only the first 3 values
-            if (count <= 2){
-                work_sums[i] += int_token;
-            }     
-            count++;
-        }
-        free(tofree);
-        count = 0;
-        // Don't sleep on the last loop
         if (i != 1){
             sleep(1);
         }
@@ -94,7 +93,7 @@ int main(){
 
     get_time(&entry);
     cpu_usage(&entry);
-    
-    printf("%s %s %d\n",
+
+    printf("%s %s %d%%\n",
             entry.date, entry.time, entry.cpuuse);
 }
