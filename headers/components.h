07fb2d57620874996baa69260e1c6bab014b4618
potato <lanveble08@gmail.com>
2022-10-08 20:29:13 +0200

    Moved headers to their own dir

---
+++ headers/components.h
@@ -0,0 +1,9 @@
+#ifndef COMP_HEADER
+#define COMP_HEADER
+
+#include "defs.h"
+
+void cpu_usage(log_entry *entry);
+void get_time(log_entry *entry);
+
+#endif
---
+++ headers/defs.h
@@ -0,0 +1,19 @@
+#ifndef DEFS_HEADER
+#define DEFS_HEADER
+
+#define SIZE 64
+
+typedef struct _log_entry {
+    char *date;
+    char *time;
+    unsigned int cpuuse;
+    unsigned int ramuse;
+    unsigned int bat;
+    char *uptime;
+} log_entry;
+
+int asprintf(char **strp, const char *fmt, ...);
+
+int pscanf(const char *path, const char *fmt, ...);
+
+#endif
