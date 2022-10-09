04d6c76b8c596f5f9fcbd421853e6a65f30e3034
potato <lanveble08@gmail.com>
2022-10-09 11:11:28 +0200

    Comments

--- src/components/bat_usage.c
+++ src/components/bat_usage.c
@@ -3,6 +3,7 @@
 void battery_perc(log_entry *entry){
     unsigned int bat_perc;
 
+    // Get the battery percentage
     if (pscanf("/sys/class/power_supply/BAT0/capacity",
                 "%d", &bat_perc) != 1) {
         return;
